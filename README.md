9e06ba22ae89e31a914edd183660d0a37f73946a
sergio1008 <sergiobernalc@outlook.com>
2022-01-11 09:51:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ftishm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ftishm)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      apellido: new FormControl('', [Validators.required]),
+    });
+  }
+  get nombre() { return this.formulario.get('nombre'); }
+  get apellido() { return this.formulario.get('apellido'); }
+  guardar() {
+    console.log(this.formulario.value);
+  }
+}
